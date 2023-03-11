08076dac7ed60a6f6235579c33bbbd0857261d72
Meriku <m.mirik.v@gmail.com>
2023-03-11 19:58:15 +0100

    Tree visualization using Grid

--- GItClient/Core/Models/Tree.cs
+++ GItClient/Core/Models/Tree.cs
@@ -13,10 +13,13 @@ namespace GItClient.Core.Models
         public TreeNode<T> Head { get; set; }
 
         public Dictionary<string, TreeNode<T>> AllNodes { get; set; }
+        public Dictionary<int, List<TreeNode<T>>> AllNodesByGeneration { get; set; }
+
 
         public Tree() 
         {
             AllNodes = new Dictionary<string, TreeNode<T>>();
+            AllNodesByGeneration = new Dictionary<int, List<TreeNode<T>>>();
         }
 
         public void Add(T data)
@@ -27,6 +30,9 @@ namespace GItClient.Core.Models
             if (Head == null)
             {
                 Head = node;
+                node.Generation = 0;
+                AllNodesByGeneration[node.Generation] = new List<TreeNode<T>> { node };
+
                 return;
             }
             else
@@ -34,9 +40,17 @@ namespace GItClient.Core.Models
                 var parentHashes = node.GetParentHashes();
                 foreach (var hash in parentHashes)
                 {
-                    AllNodes[hash].AddChild(node);
+                    var parent = AllNodes[hash];
+                    node.AddParent(parent);
+                    parent.AddChild(node);
                 }
+                node.CalculateGeneration();
 
+                if (!AllNodesByGeneration.ContainsKey(node.Generation))
+                {
+                    AllNodesByGeneration[node.Generation] = new List<TreeNode<T>>();
+                }
+                AllNodesByGeneration[node.Generation].Add(node);
             }
         }
 
@@ -48,18 +62,29 @@ namespace GItClient.Core.Models
         public T Data { get; set; }
 
         public List<TreeNode<T>> Children { get; set; }
+        public List<TreeNode<T>> Parents { get; set; }
+
+        public int Generation { get; set; }
 
         public TreeNode(T data)
         {
             Data = data;
             Children = new List<TreeNode<T>>();
+            Parents = new List<TreeNode<T>>();
         }
 
         public void AddChild(TreeNode<T> node)
         {
             Children.Add(node);
         }
-
+        public void AddParent(TreeNode<T> node)
+        {
+            Parents.Add(node);
+        }
+        public void CalculateGeneration()
+        {
+            Generation = Parents.Count > 0 ? Parents[0].Generation + 1 : 0;
+        }
         public string GetHash()
         {
             return Data.GetHash();
--- GItClient/MVVM/View/PartialView/CommitsHistoryTreeView.xaml.cs
+++ GItClient/MVVM/View/PartialView/CommitsHistoryTreeView.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
 using GItClient.Core.Controllers;
 using GItClient.Core.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using static Microsoft.WindowsAPICodePack.Shell.PropertySystem.SystemProperties.System;
+using Task = System.Threading.Tasks.Task;
 
 namespace GItClient.MVVM.View.PartialView
 {
@@ -41,41 +47,44 @@ namespace GItClient.MVVM.View.PartialView
     
             Dispatcher.Invoke(() =>
             {
-
                 var head = tree.Head;
-                var treeHead = new TreeViewItem<GitCommitBase>(head, MainCanvas);
-                //Tree with all children are created here
+                
+                for (var i = 0; ; i++)
+                {
+                    MainGrid.RowDefinitions.Add(new RowDefinition());
 
-                var X = (int)MainCanvas.ActualWidth / 2;
-                var Y = 0;
+                    if (!tree.AllNodesByGeneration.ContainsKey(i))
+                    {
+                        break;
+                    }
 
-                Canvas.SetLeft(treeHead.Body, X);
-                Canvas.SetTop(treeHead.Body, Y);
+                    var nodes = tree.AllNodesByGeneration[i];
 
-                MainCanvas.Height = 3000;
+                    for (var n = 0; n < nodes.Count; n++)
+                    {
+                        if (MainGrid.ColumnDefinitions.Count <= n)
+                        {
+                            MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                        }
 
-                SetTreeItemsPosition(treeHead, X, Y);
+                        var viewNode = new TreeViewItem<GitCommitBase>(nodes[n]);
+
+                        MainGrid.Children.Add(viewNode.Body);
+                        Grid.SetRow(viewNode.Body, i);
+                        Grid.SetColumn(viewNode.Body, n);
+
+                    }
+                    
+
+
+
+                }
+
             });
         }
 
-        private void SetTreeItemsPosition(TreeViewItem<GitCommitBase> treeNode, int x, int y)
-        {
-            var X = x;
-            var Y = y;
 
-            Y += 30;
-            X -= treeNode.Children.Length * 20;
 
-            foreach (var child in treeNode.Children)
-            {
-                Canvas.SetLeft(child.Body, X);
-                Canvas.SetTop(child.Body, Y);
-
-                X += 20;
-
-                SetTreeItemsPosition(child, X, Y);
-            }
-        }
     }
 }
 
@@ -83,20 +92,24 @@ public class TreeView<T> where T : IGetHash, IGetParentHashes
 {
     public TreeViewItem<T> Head { get; set; }
 
+    public Dictionary<string, TreeViewItem<T>> AllNodes { get; set; }
     public TreeView()
     {
-
+        AllNodes = new Dictionary<string, TreeViewItem<T>>();
     }
 }
 
 public class TreeViewItem<T> where T : IGetHash, IGetParentHashes
 {
-    public TreeViewItem<T>[] Children { get; set; }
     public Line[] Edges { get; set; }
     public Ellipse Body { get; set; }
     public TreeNode<T> Data { get; set; }
+    public Point Location { get; set; }
 
-    public TreeViewItem(TreeNode<T> data, Canvas canvas)
+    public int RowIndex { get; set; }
+    public int ColumnIndex { get; set; }
+
+    public TreeViewItem(TreeNode<T> data)
     {
         Data = data;
 
@@ -105,14 +118,11 @@ public class TreeViewItem<T> where T : IGetHash, IGetParentHashes
         Body.Width = 20;
         Body.Fill = new SolidColorBrush(Colors.White);
 
-        canvas.Children.Add(Body);
+        Body.ToolTip = new ToolTip() { Content = Data.GetHash(), Foreground = new SolidColorBrush(Colors.Black) };     
+    }
 
-        var children = new List<TreeViewItem<T>>(Data.Children.Count);
-        foreach (var child in Data.Children)
-        {
-            children.Add(new TreeViewItem<T>(child, canvas));
-        }
-        Children = children.ToArray();
-
+    public string GetHash()
+    {
+        return Data.Data.GetHash();
     }
 }
